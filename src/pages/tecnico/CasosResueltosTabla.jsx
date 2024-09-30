5896f4acd1bbdb7fe3e05c0bc77465d16b198b71
Marcos-dorado <marcosdoradoh@gmail.com>
2024-09-30 12:41:21 -0500

    se agregaron campos

--- src/pages/tecnico/CasosResueltosTabla.jsx
+++ src/pages/tecnico/CasosResueltosTabla.jsx
@@ -40,6 +40,57 @@ const CasosResueltosTabla = () => {
     c.descripcion.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  // const columns = [
+  //   {
+  //     name: 'Código del Caso',
+  //     selector: row => row.codigoCaso,
+  //     cell: row => (
+  //       <div className="break-words overflow-hidden max-w-xs">{row.codigoCaso || 'No disponible'}</div>
+  //     ),
+  //   },
+  //   {
+  //     name: 'Descripción',
+  //     selector: row => row.descripcion,
+  //     cell: row => (
+  //       <div className="break-words overflow-hidden max-w-xs">{row.descripcion || 'No disponible'}</div>
+  //     ),
+  //   },
+  //   {
+  //     name: 'Fecha de Resolución',
+  //     selector: row => row.fecha, // Asegúrate de que 'createdAt' sea el campo correcto para la fecha de resolución
+  //     cell: row => (
+  //       <div className="break-words overflow-hidden max-w-xs">{row.fecha || 'No disponible'}</div>
+  //     ),
+  //   },
+  //   {
+  //     name: 'Solución',
+  //     selector: row => row.solucion.descripcionSolucion,
+  //     cell: row => (
+  //       <div className="break-words overflow-hidden max-w-xs">
+  //         {row.solucion.descripcionSolucion ? row.solucion.descripcionSolucion : 'No disponible'}
+  //       </div>
+  //     ),
+  //     width: '200px', 
+  //   },
+  //   {
+  //     name: 'Evidencia',
+  //     cell: (row) =>
+  //       row.solucion.evidencia ? (
+  //         <a
+  //           href={row.solucion.evidencia.url}
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //           className="text-blue-500 hover:underline break-words overflow-hidden max-w-xs"
+  //         >
+  //           Ver Foto
+  //         </a>
+  //       ) : (
+  //         <div className="break-words overflow-hidden max-w-xs">Sin Evidencia</div>
+  //       ),
+  //   },
+    
+  // ];
+  
   const columns = [
     {
       name: 'Código del Caso',
@@ -49,6 +100,22 @@ const CasosResueltosTabla = () => {
       ),
     },
     {
+      name: 'Foto del Caso',
+      cell: row =>
+        row.foto && row.foto.url ? (
+          <a
+            href={row.foto.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:underline break-words overflow-hidden max-w-xs"
+          >
+            Ver Foto del Caso
+          </a>
+        ) : (
+          <div className="break-words overflow-hidden max-w-xs">Sin Foto</div>
+        ),
+    },
+    {
       name: 'Descripción',
       selector: row => row.descripcion,
       cell: row => (
@@ -56,10 +123,26 @@ const CasosResueltosTabla = () => {
       ),
     },
     {
-      name: 'Fecha de Resolución',
-      selector: row => row.fecha, // Asegúrate de que 'createdAt' sea el campo correcto para la fecha de resolución
+      name: (
+        <div className="whitespace-normal">Fecha de Resolución</div>),
+      selector: row => row.fecha, // Asegúrate de que 'fecha' sea el campo correcto
       cell: row => (
         <div className="break-words overflow-hidden max-w-xs">{row.fecha || 'No disponible'}</div>
+      ),
+    },
+    {
+      name: 'Usuario',
+      selector: row => row.usuario ? row.usuario.nombre : 'No disponible',
+      cell: row => (
+        <div className="break-words overflow-hidden max-w-xs">{row.usuario ? row.usuario.nombre : 'No disponible'}</div>
+      ),
+    },
+    
+    {
+      name: 'Ambiente',
+      selector: row => row.ambiente.nombre, // Accede al nombre del ambiente (populate)
+      cell: row => (
+        <div className="break-words overflow-hidden max-w-xs">{row.ambiente.nombre || 'No disponible'}</div>
       ),
     },
     {
@@ -74,7 +157,7 @@ const CasosResueltosTabla = () => {
     },
     {
       name: 'Evidencia',
-      cell: (row) =>
+      cell: row =>
         row.solucion.evidencia ? (
           <a
             href={row.solucion.evidencia.url}
@@ -88,10 +171,9 @@ const CasosResueltosTabla = () => {
           <div className="break-words overflow-hidden max-w-xs">Sin Evidencia</div>
         ),
     },
-    
+
   ];
   
-
 
   
 
